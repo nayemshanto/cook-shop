213675251e1d438a3f5d0b26323356ce4be12262
nayemshanto333 <nayemshantobd@gamil.com>
2025-03-21 23:10:08 +0600

    Navbar done

---
+++ src/app/(withnav)/favorites/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const favoritePage = () => {
+  return (
+    <div>favoritePage</div>
+  )
+}
+
+export default favoritePage
--- src/components/shared/navbar/index.tsx
+++ src/components/shared/navbar/index.tsx
@@ -1,34 +1,51 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
+const navItem = [
+  {
+    label: "Home",
+    link: "/",
+  },
+  {
+    label: "Favorites",
+    link: "/favorites",
+  },
+  {
+    label: "Profile",
+    link: "/profile",
+  },
+];
+
 const Navbar = () => {
-  const navItem = [
-    {
-      label: "Home",
-      link: "/",
-    },
-    {
-      label: "Favorites",
-      link: "/favorites",
-    },
-    {
-      label: "Profile",
-      link: "/profile",
-    },
-  ];
-  const className = "uppercase text-sm active:font-semibold"
+  const className =
+    "uppercase text-sm active:font-medium pb-[20px] inline-block ";
+  const pathName = usePathname();
 
   return (
     <div className="font-Fredoka">
-      <nav className="flex justify-between items-center w-full max-w-[1366px] mx-auto px-5 pt-[30px] pb-5">
+      <nav className="flex justify-between items-center w-full max-w-[1366px] mx-auto px-5 pt-[30px] ">
         <div className="cursor-pointer">
           <Image alt="logo" src="/logo.png" width={200} height={20} />
         </div>
-        <div className="flex items-center space-x-[60px]">
-          <div className="space-x-[75px]">
+        <div className="flex items-center justify-between gap-5 ">
+          <div className=" ">
             {navItem.map(({ label, link }) => (
-              <Link className={className} href={link} key={label}>{label}</Link>
+              <Link
+                className={cn(
+                  className,
+                  "px-[37.5px]",
+                  pathName === link && "font-medium border-b-2 border-[#ffa700]"
+                )}
+                href={link}
+                key={label}
+              >
+                {label}
+              </Link>
             ))}
           </div>
           <button className={`cursor-pointer ${className}`}>Logout</button>
